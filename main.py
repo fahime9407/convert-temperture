741ef8abe42ae7254877a8217e4559504b391e51
fahime <fahime.emlaei@gmail.com>
2025-03-27 21:52:03 +0330

    update main

--- main.py
+++ main.py
@@ -5,22 +5,31 @@ temperature = ''
 current_unit = ''
 requested_unit = ''
 
+# when convert button press
+def convert_button_press(event):
+    pass
+# create a window
 root = Tk()
 root.geometry("235x150")
 root.title("temperature")
 # temperature
 lable_1 = Label(root, text= "temperature : ").grid(row= 0, column= 0)
-e1 = Entry(root).grid(row= 0, column= 1)
+e1 = Entry(root)
+e1.grid(row= 0, column= 1)
 # current unit
 label2 = Label(root, text="current unit   : ").place(x= 0, y= 30)
-combo_box1 = ttk.Combobox(root, values=["F", "K", "C"], width= 5).place(x= 80, y= 30)
+combo_box1 = ttk.Combobox(root, values=["F", "K", "C"], width= 5)
+combo_box1.place(x= 80, y= 30)
 # requested unit
 label3 = Label(root, text="requested unit : ").place(x= 0, y= 60)
-combo_box2 = ttk.Combobox(root, values=["F", "K", "C"], width= 5).place(x= 90, y= 60)
-# convert button
-button1 = Button(root, text='convert', width= 15, command= root.destroy).place(x= 110, y= 95)
+combo_box2 = ttk.Combobox(root, values=["F", "K", "C"], width= 5)
+combo_box2.place(x= 90, y= 60)
 # show result
-label2 = Label(root, text= "Hello").place(x= 117, y= 130)
+label_result = Label(root, text= "result : --- ")
+label_result.place(x= 0, y= 130)
+# convert button
+#button = Button(root, text='convert', width= 15, command= convert_button_press)
+#button.place(x= 110, y= 95)
 
 root.mainloop()
 
