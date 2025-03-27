b1bf0bd783062fe50455b491d76ee1b13dd88fbd
fahime <fahime.emlaei@gmail.com>
2025-03-27 20:33:10 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-temperature = float(input("Enter temperature : "))
-current_unit = input("Enter current unit : ")
-requested_unit = input("Eneter the unit you want : ")
+from tkinter import *
+from tkinter import ttk
+
+temperature = ''
+current_unit = ''
+requested_unit = ''
+
+root = Tk()
+root.geometry("235x150")
+root.title("temperature")
+# temperature
+lable_1 = Label(root, text= "temperature : ").grid(row= 0, column= 0)
+e1 = Entry(root).grid(row= 0, column= 1)
+# current unit
+label2 = Label(root, text="current unit   : ").place(x= 0, y= 30)
+combo_box1 = ttk.Combobox(root, values=["F", "K", "C"], width= 5).place(x= 80, y= 30)
+# requested unit
+label3 = Label(root, text="requested unit : ").place(x= 0, y= 60)
+combo_box2 = ttk.Combobox(root, values=["F", "K", "C"], width= 5).place(x= 90, y= 60)
+# convert button
+button1 = Button(root, text='convert', width= 15, command= root.destroy).place(x= 110, y= 95)
+# show result
+label2 = Label(root, text= "Hello").place(x= 117, y= 130)
+
+root.mainloop()
 
 if current_unit == 'C' and requested_unit == 'F' :
     converted_unit =  (temperature * (9/5)) + 32
